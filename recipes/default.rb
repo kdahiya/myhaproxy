9807d6b79f5e26b46ae785c6b773cfe0c3817d78
kdahiya <dahiyak@yahoo.com>
2017-11-03 14:13:57 +0000

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+node.default['haproxy']['members'] = [{
+	'hostname' => 'ec2-34-235-145-71.compute-1.amazonaws.com',
+	'ipaddress' => '34.235.145.71',
+	'port' => 80,
+	'ssl_port' => 80
+}]
+
+
+include_recipe 'haproxy::manual'
